89e3328fea1c1de3bfc047184c44d9a9bc7aba33
Tyler Pickett <t.pickett66@gmail.com>
2013-07-15 19:01:02 -0500

    set up our interpreter
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- lib/ruby_script/interpreter.rb
+++ lib/ruby_script/interpreter.rb
@@ -1,7 +1,13 @@
 require 'sexp_processor'
+require 'rkelly'
 
 module RubyScript
-  class Interpreter < SexpProcessor
+  class Interpreter < SexpInterpreter
+    attr_reader :parser
 
+    def initialize
+      super
+      @parser = RKelly::Parser.new
+    end
   end
 end
