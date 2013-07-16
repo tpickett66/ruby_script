41be584fda9e1d3590dd4f5e1768a84137332c67
Tyler Pickett <t.pickett66@gmail.com>
2013-07-15 19:00:41 -0500

    implement sanity check on basic arithmetic
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

--- test/ruby_script/interpreter_test.rb
+++ test/ruby_script/interpreter_test.rb
@@ -2,6 +2,21 @@ require 'test_helper'
 
 module RubyScript
   class InterpreterTest < Minitest::Unit::TestCase
+    attr_accessor :int
 
+    def setup
+      self.int = Interpreter.new
+    end
+
+    def test_sanity
+      assert_eval 3, '3'
+      assert_eval 9, '6 + 3;'
+    end
+
+    private
+
+    def assert_eval(expression, source, message = nil)
+      assert_equal expression, int.eval(source), message
+    end
   end
 end
