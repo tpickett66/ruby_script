30ab2a2dfe412aae898251f7e067ad3f2c90ce04
Tyler Pickett <t.pickett66@gmail.com>
2013-08-20 21:16:47 -0500

    add a couple more slides including the first code sample
    Signed-off-by: Tyler Pickett <t.pickett66@gmail.com>

---
+++ lib/code_example/0.rb
@@ -0,0 +1,20 @@
+# interpreter.rb
+require 'sexp_processor'
+require 'rkelly'
+
+class Interpreter < SexpInterpreter
+  attr_reader :parser
+
+  def initialize
+    super
+    @parser = RKelly::Parser.new
+  end
+
+  def eval(source)
+  end
+end
+
+# interpreter_test.rb
+require 'test_helper'
+class RubyScriptTest < Minitest::Test
+end
